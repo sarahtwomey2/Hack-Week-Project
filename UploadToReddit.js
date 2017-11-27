54cf9a80dc9cdbb14e6bf07d06aa0764278fcdab
sarahtwomey2 <31322452+sarahtwomey2@users.noreply.github.com>
2017-11-26 22:52:55 -0600

    Add files via upload

---
+++ UploadToReddit.js
@@ -0,0 +1,21 @@
+//var snoowrap = require('snoowrap');
+
+
+function upload() {
+                                  
+    
+    // Alternatively, just pass in a username and password for script-type apps.
+    const r = new snoowrap({
+        userAgent: 'SarahBot 1.0 by /u/ meme_zou',
+        clientId: 'ZVvTYMie6R2hrQ',
+        clientSecret: 'IvlgET7VrpM73YkOYCdPl8nTwS8',
+        username: 'meme_zou',
+        password: 'CS4320'
+    });
+                    
+    // Submitting a link to a subreddit
+    r.getSubreddit('memezou').submitLink({
+        title: 'https://i.imgur.com/n5iOc72.gifv',
+        url: 'https://i.imgur.com/n5iOc72.gifv'
+    });
+}
